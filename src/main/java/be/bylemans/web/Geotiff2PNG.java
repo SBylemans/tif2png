b44cc4429359e712d634c07acd72ea7f51d84aee
sbylemans <sander.bylemans@continuum.be>
2020-09-11 12:28:14 +0200

    WMS + tif

--- src/main/java/be/bylemans/web/Geotiff2PNG.java
+++ src/main/java/be/bylemans/web/Geotiff2PNG.java
@@ -4,12 +4,15 @@ import java.awt.image.RenderedImage;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Map;
 import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.Produces;
 import org.geotools.coverage.grid.GridCoverage2D;
 import org.geotools.gce.geotiff.GeoTiffReader;
 import org.geotools.util.factory.Hints;
+import org.opengis.geometry.Envelope;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,12 +29,48 @@ public class Geotiff2PNG {
     convertToPng(in, response.getOutputStream());
   }
 
-  static void convertToPng(InputStream inputStream, OutputStream outputStream) throws IOException {
+  @GetMapping(value = "/{imageName}/metadata")
+  @Produces(MediaType.IMAGE_PNG_VALUE)
+  public Map<String, Object> getImageMetadataWithName(@PathVariable String imageName, HttpServletResponse response)
+      throws IOException {
+    InputStream in = getClass().getResourceAsStream("/images/" + imageName + ".tif");
+    return imageMetadata(in);
+  }
+
+  static Map<String, Object> convertToPng(InputStream inputStream, OutputStream outputStream) throws IOException {
     GeoTiffReader reader = new GeoTiffReader(inputStream,
         new Hints(Hints.FORCE_LONGITUDE_FIRST_AXIS_ORDER, Boolean.TRUE));
     GridCoverage2D coverage = reader.read(null);
     RenderedImage image = coverage.getRenderedImage();
     ImageIO.write(image, "PNG", outputStream);
+
+    Map<String, Object> result = new HashMap<>();
+    Envelope env = coverage.getEnvelope();
+
+    double minX = env.getLowerCorner().getOrdinate(0);
+    double minY = env.getLowerCorner().getOrdinate(1);
+
+    double maxX = env.getUpperCorner().getOrdinate(0);
+    double maxY = env.getUpperCorner().getOrdinate(1);
+    result.put("extent", new double[]{minX, minY, maxX, maxY});
+    return result;
+  }
+
+  static Map<String, Object> imageMetadata(InputStream inputStream) throws IOException {
+    GeoTiffReader reader = new GeoTiffReader(inputStream,
+        new Hints(Hints.FORCE_LONGITUDE_FIRST_AXIS_ORDER, Boolean.TRUE));
+    GridCoverage2D coverage = reader.read(null);
+
+    Map<String, Object> result = new HashMap<>();
+    Envelope env = coverage.getEnvelope();
+
+    double minX = env.getLowerCorner().getOrdinate(0);
+    double minY = env.getLowerCorner().getOrdinate(1);
+
+    double maxX = env.getUpperCorner().getOrdinate(0);
+    double maxY = env.getUpperCorner().getOrdinate(1);
+    result.put("extent", new double[]{minX, minY, maxX, maxY});
+    return result;
   }
 
 }
--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -1,35 +1,99 @@
-
 import ImageLayer from 'ol/layer/Image';
 import Map from 'ol/Map';
-import Projection from 'ol/proj/Projection';
 import Static from 'ol/source/ImageStatic';
 import View from 'ol/View';
-import {getCenter} from 'ol/extent';
+import TileLayer from "ol/layer/Tile";
+import proj4 from "proj4";
+import * as extent from 'ol/extent.js';
+import * as proj from 'ol/proj.js';
+import * as olProj4 from 'ol/proj/proj4.js';
+import WMTSCapabilities from "ol/format/WMTSCapabilities";
+import * as wmts from "ol/source/WMTS";
+import WMTS from "ol/source/WMTS";
 
-// Map views always need a projection.  Here we just want to map image
-// coordinates directly to map coordinates, so we create a projection that uses
-// the image extent in pixels.
-    var extent = [0, 0, 1024, 968];
-    var projection = new Projection({
-      code: 'xkcd-image',
-      units: 'pixels',
-      extent: extent,
-    });
+const BELGIAN_LAMBERT_72_PROJECTION = 'EPSG:31370';
+
+const PROJ4_DEFINITION = '+proj=lcc +lat_1=51.16666723333333 +lat_2=49.8333339 +lat_0=90 +lon_0=4.367486666666666 +x_0=150000.013 +y_0=5400088.438 +ellps=intl +towgs84=106.869,-52.2978,103.724,-0.33657,0.456955,-1.84218,1 +units=m +no_defs';
+
+proj4.defs(BELGIAN_LAMBERT_72_PROJECTION, PROJ4_DEFINITION);
+
+olProj4.register(proj4);
+
+var view = new View({
+  center: [140860.69299028325, 190532.7165957574],
+  extent: [9928.000000, 66928.000000, 272072.000000, 329072.000000],
+  projection: proj.get('EPSG:31370'),
+  resolutions: _createResolutions(
+      [9928.000000, 66928.000000, 272072.000000, 329072.000000]),
+  zoom: 2,
+});
+
+var parser = new WMTSCapabilities();
+var wmtsUrl = "http://tile.informatievlaanderen.be/ws/raadpleegdiensten/wmts";
+
+var wmtsCapabilities;
+Promise.all(
+    [fetch(wmtsUrl + "?request=getcapabilities&service=wmts&version=1.0.0"),
+      fetch("/test/metadata")]).then(([response, metadata]) => {
+
+  Promise.all([response.text(), metadata.json()]).then(([text, json]) => {
+    wmtsCapabilities = parser.read(text);
+
+    var layer1 = _createTileLayer(
+        _loadWMTSOptions(wmtsCapabilities, "grb_bsk_grijs", "BPL72VL"),
+        0);
+
+    var layer2 = _createImageLayer(json, 1)
 
     var map = new Map({
-      layers: [
-        new ImageLayer({
-          source: new Static({
-            url: '/test',
-            projection: projection,
-            imageExtent: extent,
-          }),
-        }) ],
+      layers: [layer1, layer2],
       target: 'map',
-      view: new View({
-        projection: projection,
-        center: getCenter(extent),
-        zoom: 2,
-        maxZoom: 8,
-      }),
+      view: view
     });
+  })
+})
+
+function _createTileLayer(options, zIndex) {
+  options.crossOrigin = 'anonymous';
+  return new TileLayer({
+    visible: true,
+    source: new WMTS(options),
+    zIndex: zIndex,
+  });
+}
+
+function _createImageLayer(options, zIndex) {
+  return new ImageLayer({
+    visible: true,
+    source: new Static({
+      url: '/test',
+      imageExtent: options.extent,
+    }),
+    zIndex: zIndex,
+  });
+}
+
+function _loadWMTSOptions(capabilities, layerName, matrixSet) {
+  const wmtsOptions = wmts.optionsFromCapabilities(capabilities, {
+    layer: layerName,
+    matrixSet: matrixSet,
+  });
+  _updateWMTSOptions(wmtsOptions);
+  return wmtsOptions;
+}
+
+function _updateWMTSOptions(options) {
+  options.crossOrigin = 'anonymous';
+}
+
+function _createResolutions(bbox) {
+  const MAX_RESOLUTION = extent.getHeight(bbox) / 256;
+
+  // Generate a sequence of numbers
+  // Since the array is initialized with `undefined` on each position,
+  // the value of `elementValue` below will be `undefined`
+  return Array.from(Array(16),
+      (elementValue, elementIndex) => MAX_RESOLUTION / Math.pow(2,
+          elementIndex));
+}
+
